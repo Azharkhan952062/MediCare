c1c66ff7951485b1d9e2ca6dbeeb01c6100aff21
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-06-01 17:43:58 +0530

    Use env for MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://azruddinkhan952062_db_user:F5Id26Yd9jtqJT64@cluster0.20vctbo.mongodb.net/MediCare")
+  await mongoose
+    .connect(process.env.MONGO_URI)
     .then(() => {
-        console.log("DB CONNECTED")
-    })
-}
+      console.log("DB CONNECTED");
+    });
+};
